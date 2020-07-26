87d74d1fda054a67dc1efe50d7c98161568103c4
lechu0715 <lechu0715@gmail.com>
2020-07-26 17:23:27 +0200

    Add Project files

---
+++ Tydzien4Lekcja12/FizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tydzien4Lekcja12
+{
+    class FizzBuzz
+    {
+        public string Game(int value)
+        {
+            
+            if (value % 3 == 0 && value % 5 == 0)
+            {
+                return "FizBuz";
+            }
+            else if (value % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (value % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+    }
+
+    
+
+}
---
+++ Tydzien4Lekcja12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tydzien4Lekcja12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FizzBuzz fizzBuzz = new FizzBuzz();
+
+                Console.WriteLine("Podaj liczbę całkowitą:");
+                var number = int.Parse(Console.ReadLine());
+                var score = fizzBuzz.Game(number);
+                Console.WriteLine(score);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+                Console.WriteLine("Podana wartość nie jest liczbą całkowitą");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
